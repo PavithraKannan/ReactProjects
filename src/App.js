e2e03f78bdc071bb16541fe4325cc63fca456c89
PavithraKannan <paaveethra@gmail.com>
2023-06-07 23:22:53 +0300

    installed fluent ui northstar

--- src/App.js
+++ src/App.js
@@ -1,54 +1,39 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// function App() {
-//   const [quote, setQuote] = useState('');
-//   const handleRefreshClick = () => {
-//     window.location.reload();
-//   };
-
-//   useEffect(() => {
-//     const fetchQuote = async () => {
-//       try {
-//         const response = await axios.get('https://api.breakingbadquotes.xyz/v1/quotes/5');
-//         setQuote(response.data[0].quote);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-
-//     fetchQuote();
-//   }, []);
-
-
-//   return (
-//     <Card sx={{ minWidth: 275 }}>
-//       <div className="App">
-//         <h1>Breaking Bad Quotes</h1>
-//         <p>{quote}</p>
-//         <button onClick={handleRefreshClick}>Refresh</button>
-//       </div>
-//     </Card>
-
-//   );
-// }
-
-// export default App;
-
-
-import React from 'react';
-import { TextField } from '@fluentui/react';
+import React, { useState, useEffect } from 'react';
+import { Card } from '@fluentui/react';
+import axios from 'axios';
 
 function App() {
-  const handleInputChange = (event, newValue) => {
-    console.log(newValue);
+  const [quote, setQuote] = useState('');
+  const handleRefreshClick = () => {
+    window.location.reload();
   };
 
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await axios.get('https://api.breakingbadquotes.xyz/v1/quotes/5');
+        setQuote(response.data[0].quote);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchQuote();
+  }, []);
+
+
   return (
-    <div>
-      <TextField label="Enter your name" onChange={handleInputChange} />
-    </div>
+    <Card>
+      <div className="App">
+        <h1>Breaking Bad Quotes</h1>
+        <p>{quote}</p>
+        <button onClick={handleRefreshClick}>Refresh</button>
+      </div>
+    </Card>
+
   );
 }
 
 export default App;
+
+
